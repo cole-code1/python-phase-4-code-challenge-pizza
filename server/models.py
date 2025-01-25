e14a886e5af53b70cac20b8177a30815a0906b77
cole-code1 <collinskathu313@gmail.com>
2025-01-26 01:18:36 +0300

    initial commit

--- server/models.py
+++ server/models.py
@@ -21,8 +21,14 @@ class Restaurant(db.Model, SerializerMixin):
     address = db.Column(db.String)
 
     # add relationship
+    restaurants = association_proxy("restaurant_pizzas", "restaurant")
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="restaurant")
+
+
 
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas',)
+
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
@@ -37,7 +43,14 @@ class Pizza(db.Model, SerializerMixin):
 
     # add relationship
 
+    restaurants = association_proxy("restaurant_pizzas", "pizza")
+    restaurant_pizzas = db.relationship("RestaurantPizza", back_populates="pizza")
+
+    
+
     # add serialization rules
+    serialize_rules = ('-restaurant_pizzas',)
+
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
@@ -50,10 +63,20 @@ class RestaurantPizza(db.Model, SerializerMixin):
     price = db.Column(db.Integer, nullable=False)
 
     # add relationships
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"), nullable=False)
 
-    # add serialization rules
+    # add serilazation rules
+    serialize_rules = ('-restaurant', '-pizza',) 
 
     # add validation
+    @validates("price")
+    def validate_price(self, key, value):
+        if value <= 0:
+            raise ValueError("Price must be greater than 0")
+        if value > 30:
+            raise ValueError("Price must be less than or equal to 30")
+        return value
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
